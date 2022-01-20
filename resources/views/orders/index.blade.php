166e8cd988e3de7684b673fb8187d70c0bbfc3a3
sa-rx <s-ra1@outlook.com>
2022-01-20 18:40:05 +0300

    a

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -94,7 +94,7 @@
         <div   class="col-lg-12 menu-item filter-orderStatus" >
             <p>المدفوع</p>
             <div class="col-xl-12 mx-auto container  section-bg">
-               <!-- <h4>{{$orderStatusTotal_price}}</h4> -->
+              
                 <table style="--bs-table-hover-color: #d8a781 ;     border-color: #1e252c00;" class="table section-bg rounded table-responsive-xxl table-hover  text-light ">
                     <thead  class="">
                         <tr>
