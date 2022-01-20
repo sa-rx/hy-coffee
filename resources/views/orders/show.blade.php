40b8c394f1b81d597605e0434c4fdaa923b550cb
sa-rx <s-ra1@outlook.com>
2022-01-20 19:01:45 +0300

    a

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -1,31 +1,32 @@
-@extends('layouts.app')
-
-@section('content')
 
 
+<br><br>
   <!-- ======= Chefs Section ======= -->
-  <section id="chefs" class="chefs container section-bg">
+  <section id="chefs" class="chefs container section-bg" dir="rtl">
         <div  id="example" class="container " data-aos="fade-up">
 
             <div class="section-title">
                 <h2>رقم الطلب #{{$order->id}} </h2>
             </div>
             <br>
-            <h5>{!! nl2br( $order->cart )!!}</h5>
-            <hr>
+            <h4>{!! nl2br( $order->cart )!!}</h4>
+            
           
             @if( $order->status == 1)
             <h4>دفع</h4>
             @else
-            <h4 class="text-danger ">لم يدفع</h4>
+            <h2 style="color: #b70019;">لم يدفع</h2>
             @endif
-            <h5> عدد الاصناف : {{$order->total_qty}} </h5>
-            <h5>الاجمالي : {{$order->total_price}} ريال</h5>
+            <h3> عدد الاصناف : {{$order->total_qty}} </h3>
+            <h3>الاجمالي : {{$order->total_price}} ريال</h3>
            
             
            
 
         </div>
+        @if( $order->status == 1)
+        <button onclick="window.print();" class="noPrint">طباعه</button>
+        @else
+        @endif
     </section><!-- End Chefs Section -->
 
-@endsection
