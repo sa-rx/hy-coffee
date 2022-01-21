493d65701ca58a8b9bf1f2b0405e1301c111ed3f
sa-rx <s-ra1@outlook.com>
2022-01-21 15:42:11 +0300

    a

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -93,6 +93,7 @@
 
             <div   class="col-lg-12 menu-item filter-orderStatus" >
                 <p>المدفوع</p>
+                 <h4>اجمالي الطلبات المدفوعه {{$orderStatusTotal_price}} ريال</h4>
                 <div class="col-xl-12 mx-auto container  section-bg">
                 
                     <table style="--bs-table-hover-color: #d8a781 ;     border-color: #1e252c00;" class="table section-bg rounded table-responsive-xxl table-hover  text-light ">
